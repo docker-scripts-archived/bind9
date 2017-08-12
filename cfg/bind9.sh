43017fb566ae5a01dc7f0aa60f1026b05511fe3b
Dashamir Hoxha <dashohoxha@gmail.com>
2017-08-12 04:38:23 +0200

    Fix the configuration script.

--- cfg/bind9.sh
+++ cfg/bind9.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+source /host/settings.sh
+
 ### fix default options
 sed -i /etc/default/bind9 \
     -e '/^OPTIONS=/ c OPTIONS="-4 -u bind"'
@@ -9,15 +11,28 @@ sed -i /etc/default/ufw \
     -e '/^IPV6/ c IPV6=no' \
     -e 's/^IPT_MODULES=/#IPT_MODULES=/'
 
-### add ufw rules for bind9
-ufw allow from 173.244.206.26 to any port 53
-ufw allow from 88.198.106.11 to any port 53
-ufw allow from 204.42.254.5 to any port 53
+### add ufw rules for allowing axfr servers
+local server_ip
+while read line; do
+    [[ -z $line ]] && continue
+    server_ip=$(echo $line | cut -d';' -f1)
+    ufw allow from $server_ip to any port 53
+done <<< "$AXFR_SERVERS"
+ufw enable
 
-### start ufw, also on reboot
-ufw enable
-cat <<_EOF > /etc/rc.local
-#!/bin/sh -e
-ufw enable
-exit 0
+### fix config files
+sed -i /etc/bind/conf/common-domains.a \
+    -e "s/127\.0\.0\.1/$PUBLIC_IP/"
+
+cat <<_EOF > /etc/bind/conf/allow-transfer
+allow-transfer {$AXFR_SERVERS};
 _EOF
+
+cat <<_EOF > /etc/bind/conf/also-notify
+also-notify {$AXFR_SERVERS};
+_EOF
+
+> /etc/bind/conf/secondary.ns
+for ns in $SECONDARY_NS; do
+    echo "  IN  NS  ${ns}." >> /etc/bind/conf/secondary.ns
+done
--- settings.sh
+++ settings.sh
@@ -1,8 +1,23 @@
 APP=bind9
+
 IMAGE=bind9
 CONTAINER=bind9
 PORTS="53:53"
 
-### email account for sending server notifications
+### Public IP of the host.
+PUBLIC_IP=10.11.12.13
+
+### Check these:
+### https://www.buddyns.com
+### https://puck.nether.net/
+### http://networking.ringofsaturn.com/Unix/freednsservers.php
+SECONDARY_NS="b.ns.buddyns.com c.ns.buddyns.com puck.nether.net"
+AXFR_SERVERS="
+    173.244.206.26;   # a.transfer.buddyns.com
+    88.198.106.11;    # b.transfer.buddyns.com
+    204.42.254.5;     # puck.nether.net
+"
+
+### Email account for sending server notifications.
 GMAIL_ADDRESS=
 GMAIL_PASSWD=
