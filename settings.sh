7c14553ec7aad58d63681c493f4589d128fdf974
dashohoxha <dashohoxha@gmail.com>
2018-05-13 15:03:49 +0200

    Fix comments for gmail account

--- settings.sh
+++ settings.sh
@@ -18,6 +18,8 @@ AXFR_SERVERS="
     204.42.254.5;     # puck.nether.net
 "
 
-### Email account for sending server notifications.
+### Gmail account for notifications. This will be used by ssmtp.
+### You need to create an application specific password for your account:
+### https://www.lifewire.com/get-a-password-to-access-gmail-by-pop-imap-2-1171882
 GMAIL_ADDRESS=
 GMAIL_PASSWD=
