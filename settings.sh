69dd9e689d58b953ccd9e5a5940d58ba3821ba9a
Dashamir Hoxha <dashohoxha@gmail.com>
2018-11-20 10:47:18 +0100

    Use your SMTP server for sending emails.

--- settings.sh
+++ settings.sh
@@ -18,6 +18,14 @@ AXFR_SERVERS="
     204.42.254.5;     # puck.nether.net
 "
 
+### SMTP server for sending notifications. You can build an SMTP server
+### as described here:
+### https://github.com/docker-scripts/postfix/blob/master/INSTALL.md
+### Comment out if you don't have a SMTP server and want to use
+### a gmail account (as described below).
+SMTP_SERVER=smtp.example.org
+SMTP_DOMAIN=example.org
+
 ### Gmail account for notifications. This will be used by ssmtp.
 ### You need to create an application specific password for your account:
 ### https://www.lifewire.com/get-a-password-to-access-gmail-by-pop-imap-2-1171882
